50d514224dfcffba6a3f7dc13ac3d75cf4f82edb
IvayloArsov <ivayloarsov13@gmail.com>
2023-05-22 21:09:02 +0300

    ref

--- python_fundamentals_2023/functions_more_exercises/5_multiplication_sign.py
+++ python_fundamentals_2023/functions_more_exercises/5_multiplication_sign.py
@@ -1,6 +1,13 @@
 user_input = [int(input()) for _ in range(3)]
 
+
 def find_solution(number_list):
+    if any(num == 0 for num in number_list):
+        return 'zero'
+    negatives = sum(1 for num in number_list if num < 0)
+
+    return "negative" if negatives % 2 != 0 else "positive"
 
 
-#sign = lambda a: 1 if a > 0 else -1 if a < 0 else 0
+result = find_solution(user_input)
+print(result)

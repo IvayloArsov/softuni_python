169246046b248a2bdf9a318d5fd1a964aea77473
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2023-06-18 10:48:02 +0300

    ref

--- python_fundamentals_2023/objects&classes_exercises/7_articles.py
+++ python_fundamentals_2023/objects&classes_exercises/7_articles.py
@@ -6,32 +6,29 @@ class Article:
 
     def edit(self, new_content: str):
         self.content = new_content
-        return self
 
     def change_author(self, new_author: str):
         self.author = new_author
-        return self
 
     def rename(self, new_title: str):
         self.title = new_title
-        return self
 
     def __repr__(self):
         return f"{self.title} - {self.content} : {self.author}"
 
 
-article = Article(
-    "Highest Recorded Temperature",
-    "Temperatures across Europe are unprecedented, according to scientists.",
-    "Ben Turner"
-)
-article.edit(
-    "Syracuse, a city on the coast of the Italian island of Sicily, registered temperatures of 48.8 degrees Celsius"
-)
-article.rename(
-    "Temperature in Italy"
-)
-article.change_author(
-    "B. T."
-)
-print(article)
+# article = Article(
+#     "Highest Recorded Temperature",
+#     "Temperatures across Europe are unprecedented, according to scientists.",
+#     "Ben Turner"
+# )
+# article.edit(
+#     "Syracuse, a city on the coast of the Italian island of Sicily, registered temperatures of 48.8 degrees Celsius"
+# )
+# article.rename(
+#     "Temperature in Italy"
+# )
+# article.change_author(
+#     "B. T."
+# )
+# print(article)

a179b2c00173209fbf99eaea5d7bbd8eccd8c9bc
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2024-03-01 17:12:09 +0200

    ref

---
+++ js_frontend_2024/JS/jsArraysAndStrings/moreEx/ex1.js
@@ -0,0 +1,24 @@
+function login(arr) {
+  const username = arr[0];
+  const correctPassword = arr[0].split("").reverse().join("");
+  let incorrectAttempts = 0;
+
+  for (let i = 1; i <= 4; i++) {
+    if (arr[i] === correctPassword) {
+      console.log(`User ${username} logged in.`);
+      return;
+    } else {
+      if (i < 4) {
+        console.log(`Incorrect password. Try again.`);
+        incorrectAttempts++;
+      } else {
+        console.log(`User ${username} blocked!`);
+        return;
+      }
+    }
+  }
+}
+
+//login(["sunny", "rainy", "cloudy", "sunny", "not sunny"]);
+//login(["momo", "omom"]);
+login(["Acer", "login", "go", "let me in", "recA"]);

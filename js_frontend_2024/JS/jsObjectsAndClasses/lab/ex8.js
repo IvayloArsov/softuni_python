1e44764b8b72e4e380ced168fe7414d590f640e4
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2024-03-07 14:40:49 +0200

    ref

---
+++ js_frontend_2024/JS/jsObjectsAndClasses/lab/ex8.js
@@ -0,0 +1,26 @@
+function solve(arr) {
+  let cats = [];
+
+  class Cat {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    meow() {
+      console.log(`${this.name}, age ${this.age} says Meow`);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    let catData = arr[i].split(" ");
+    let name, age;
+    [name, age] = [catData[0], catData[1]];
+    cats.push(new Cat(name, age));
+  }
+
+  for (cat of cats) {
+    cat.meow();
+  }
+}
+
+solve(["Mellow 2", "Tom 5"]);
---
+++ js_frontend_2024/JS/jsObjectsAndClasses/lab/ex9.js
@@ -0,0 +1,34 @@
+function solve(input) {
+  let songs = [];
+  let numberOfSongs = input.shift();
+  let typeSong = input.pop();
+
+  class Song {
+    constructor(type, name, time) {
+      this.type = type;
+      this.name = name;
+      this.time = time;
+    }
+  }
+
+  for (let i = 0; i < numberOfSongs; i++) {
+    let [type, name, time] = input[i].split("_");
+    let song = new Song(type, name, time);
+    songs.push(song);
+  }
+
+  if (typeSong === "all") {
+    songs.forEach((i) => console.log(i.name));
+  } else {
+    let filtered = songs.filter((i) => i.type === typeSong);
+    filtered.forEach((i) => console.log(i.name));
+  }
+}
+
+solve([
+  3,
+  "favourite_DownTown_3:14",
+  "favourite_Kiss_4:16",
+  "favourite_Smooth Criminal_4:01",
+  "favourite",
+]);

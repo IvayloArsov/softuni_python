854a1d3fea27e5f2d564a2b7560f0b16037151ce
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2024-04-03 17:54:14 +0300

    ref

--- js_frontend_2024/JS/jsAsynchronousPrograming/lab/01.Bus-Stop/app.js
+++ js_frontend_2024/JS/jsAsynchronousPrograming/lab/01.Bus-Stop/app.js
@@ -1,3 +1,24 @@
 function getInfo() {
-    // TODO:
+  document.getElementById("stopName").textContent = "";
+  document.getElementById("buses").innerHTML = "";
+
+  let stopId = document.getElementById("stopId").value;
+
+  fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+      document.getElementById("stopName").textContent = data.name;
+
+      for (let busId in data.buses) {
+        let time = data.buses[busId];
+        let listItem = document.createElement("li");
+        listItem.textContent = `Bus ${busId} arrives in ${time} minutes`;
+        document.getElementById("buses").appendChild(listItem);
+      }
+    })
+    .catch(() => {
+      document.getElementById("stopName").textContent = "Error";
+    });
 }
--- js_frontend_2024/JS/jsAsynchronousPrograming/lab/02.Bus-Schedule/app.js
+++ js_frontend_2024/JS/jsAsynchronousPrograming/lab/02.Bus-Schedule/app.js
@@ -1,16 +1,38 @@
 function solve() {
-    function depart() {
-        // TODO: 
-    }
+  const baseURL = "http://localhost:3030/jsonstore/bus/schedule/";
+  let infoBox = document.querySelector(".info");
+  let departButton = document.getElementById("depart");
+  let arriveButton = document.getElementById("arrive");
 
-    async function arrive() {
-        // TODO:
-    }
+  let currentStopId = "depot";
+  let nextStop = "";
 
-    return {
-        depart,
-        arrive
-    };
+  async function depart() {
+    try {
+      const response = await fetch(baseURL + currentStopId);
+      const data = await response.json();
+      infoBox.textContent = `Next stop ${data.name}`;
+      nextStop = data.name;
+      currentStopId = data.next;
+      departButton.disabled = true;
+      arriveButton.disabled = false;
+    } catch (error) {
+      infoBox.textContent = "Error";
+      departButton.disabled = true;
+      arriveButton.disabled = true;
+    }
+  }
+
+  function arrive() {
+    infoBox.textContent = `Arriving at ${nextStop}`;
+    departButton.disabled = false;
+    arriveButton.disabled = true;
+  }
+
+  return {
+    depart,
+    arrive,
+  };
 }
 
 let result = solve();

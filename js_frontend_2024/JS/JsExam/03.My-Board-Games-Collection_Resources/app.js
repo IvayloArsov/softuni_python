f17f8be8d5e1914f7cd488c00852dece72bc5089
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2024-04-13 20:54:30 +0300

    ref
    went on a fool's errand and found my error. feelsbadman.jpeg

--- js_frontend_2024/JS/JsExam/03.My-Board-Games-Collection_Resources/app.js
+++ js_frontend_2024/JS/JsExam/03.My-Board-Games-Collection_Resources/app.js
@@ -1,56 +1,108 @@
-async function loadGames() {
-  const response = await fetch(baseUrl);
-  const data = await response.json();
+document.addEventListener("DOMContentLoaded", () => {
+  const baseUrl = "http://localhost:3030/jsonstore/games/";
   const gamesList = document.getElementById("games-list");
-  gamesList.innerHTML = "";
-  Object.keys(data).forEach((key) => displayGame(data[key], key));
-}
+  const addButton = document.getElementById("add-game");
+  const editButton = document.getElementById("edit-game");
+  const gameNameInput = document.getElementById("g-name");
+  const gameTypeInput = document.getElementById("type");
+  const gamePlayersInput = document.getElementById("players");
+  const loadButton = document.getElementById("load-games");
 
-function displayGame(game, id) {
-  const gamesList = document.getElementById("games-list");
-  const div = document.createElement("div");
-  div.className = "board-game";
-  div.innerHTML = `
-      <div class="content">
-        <p>${game.name}</p>
-        <p>${game.type}</p>
-        <p>${game.players}</p>
-      </div>
-      <div class="buttons-container">
-        <button class="change-btn">Change</button>
-        <button class="delete-btn">Delete</button>
-      </div>
-    `;
-  const changeBtn = div.querySelector(".change-btn");
-  changeBtn.addEventListener("click", () => setupEdit(game, id));
-  const deleteBtn = div.querySelector(".delete-btn");
-  deleteBtn.addEventListener("click", () => deleteGame(id));
-  gamesList.appendChild(div);
-}
+  loadButton.addEventListener("click", loadGames);
+  addButton.addEventListener("click", addGame);
+  editButton.addEventListener("click", editGame);
 
-async function editGame(id) {
-  let gameNameInput = document.getElementById("g-name");
-  let gameTypeInput = document.getElementById("type");
-  let gamePlayersInput = document.getElementById("players");
+  let currentEditingId = null;
 
-  let gameData = {
-    name: gameNameInput.value,
-    type: gameTypeInput.value,
-    players: gamePlayersInput.value,
-  };
+  function loadGames() {
+    fetch(baseUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        gamesList.innerHTML = "";
+        Object.keys(data).forEach((key) => {
+          displayGame(data[key], key);
+        });
+      });
+  }
 
-  await fetch(`${baseUrl}${id}`, {
-    method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(gameData),
-  });
-  await loadGames();
-  clearForm();
-}
+  function displayGame(game, id) {
+    const div = document.createElement("div");
+    div.className = "board-game";
+    div.innerHTML = `
+        <div class="content">
+          <p>${game.name}</p>
+          <p>${game.type}</p>
+          <p>${game.players}</p>
+        </div>
+        <div class="buttons-container">
+          <button class="change-btn">Change</button>
+          <button class="delete-btn">Delete</button>
+        </div>
+      `;
+    const changeBtn = div.querySelector(".change-btn");
+    changeBtn.addEventListener("click", () => setupEdit(game, game._id));
+    const deleteBtn = div.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", () => deleteGame(game._id));
+    gamesList.appendChild(div);
+  }
 
-async function deleteGame(id) {
-  await fetch(`${baseUrl}${id}`, {
-    method: "DELETE",
-  });
-  await loadGames();
-}
+  function addGame() {
+    const gameData = {
+      name: gameNameInput.value,
+      type: gameTypeInput.value,
+      players: gamePlayersInput.value,
+    };
+
+    fetch(baseUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(gameData),
+    }).then(() => {
+      clearForm();
+      loadGames();
+    });
+  }
+
+  function clearForm() {
+    gameNameInput.value = "";
+    gameTypeInput.value = "";
+    gamePlayersInput.value = "";
+    editButton.disabled = true;
+    addButton.disabled = false;
+    currentEditingId = null;
+  }
+
+  function setupEdit(game, id) {
+    gameNameInput.value = game.name;
+    gameTypeInput.value = game.type;
+    gamePlayersInput.value = game.players;
+    editButton.disabled = false;
+    addButton.disabled = true;
+    currentEditingId = id;
+  }
+
+  function editGame() {
+    const gameData = {
+      name: gameNameInput.value,
+      type: gameTypeInput.value,
+      players: gamePlayersInput.value,
+    };
+
+    fetch(`${baseUrl + currentEditingId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(gameData),
+    }).then(() => {
+      clearForm();
+      loadGames();
+    });
+  }
+
+  function deleteGame(id) {
+    fetch(`${baseUrl + id}`, {
+      method: "DELETE",
+    }).then(() => {
+      loadGames();
+    });
+  }
+});

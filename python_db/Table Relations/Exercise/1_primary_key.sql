290d50a55087bfc5a8ee4c9075b022676c526559
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2024-06-14 13:18:23 +0300

    ref

---
+++ python_db/Table Relations/Exercise/1_primary_key.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE products (
+	product_name VARCHAR(100)
+);
+
+INSERT INTO 
+	products
+VALUES 
+	('Broccoli'),
+	('Shampoo'),
+	('Toothpaste'),
+	('Candy');
+
+ALTER TABLE
+	products
+ADD COLUMN
+	"id" SERIAL PRIMARY KEY;

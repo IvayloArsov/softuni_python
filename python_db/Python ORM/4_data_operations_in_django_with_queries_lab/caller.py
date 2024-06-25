2315690aad08d16680b9d28109e9bfc20aa3755a
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2024-06-25 13:15:38 +0300

    ref

---
+++ python_db/Python ORM/4_data_operations_in_django_with_queries_lab/caller.py
@@ -0,0 +1,44 @@
+import os
+import django
+from datetime import date, datetime
+
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+
+# Import your models here
+# Run and print your queries
+from main_app.models import Student
+
+
+def add_students():
+    student_data = [
+        ('FC5204', 'John', 'Doe', '15/05/1995', 'john.doe@university.com'),
+        ('FE0054', 'Jane', 'Smith', 'NULL', 'jane.smith@university.com'),
+        ('FH2014', 'Alice', 'Johnson', '10/02/1998', 'alice.johnson@university.com'),
+        ('FH2015', 'Bob', 'Wilson', '25/11/1996', 'bob.wilson@university.com')
+    ]
+
+    for student in student_data:
+        student_id, first_name, last_name, birth_date, email = student
+
+        if birth_date != 'NULL':
+            birth_date = datetime.strptime(birth_date, '%d/%m/%Y').date()
+        else:
+            birth_date = None
+
+        student_obj = Student(
+            student_id=student_id,
+            first_name=first_name,
+            last_name=last_name,
+            birth_date=birth_date,
+            email=email
+        )
+
+        student_obj.save()
+
+
+add_students()
+print(Student.objects.all())

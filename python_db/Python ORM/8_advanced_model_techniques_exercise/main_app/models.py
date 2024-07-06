2dc5b8c4dd0f977a8ebba99198f8aea8b792db1a
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2024-07-06 17:50:36 +0300

    ref

--- python_db/Python ORM/8_advanced_model_techniques_exercise/main_app/models.py
+++ python_db/Python ORM/8_advanced_model_techniques_exercise/main_app/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxLengthValidator, MinValueValidator, EmailValidator, URLValidator, \
     MinLengthValidator
@@ -108,3 +110,83 @@ class Music(BaseMedia):
             MinLengthValidator(9, 'Artist must be at least 9 characters long')
         ]
     )
+
+
+# Tax Inclusive Pricing Task
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def calculate_tax(self):
+        return self.price * Decimal('0.08')
+
+    def calculate_shipping_cost(self, weight: Decimal):
+        return weight * Decimal('2.00')
+
+    def format_product_name(self):
+        return f'Product: {self.name}'
+
+
+class DiscountedProduct(Product):
+    class Meta:
+        proxy = True
+
+    def calculate_price_without_discount(self):
+        price_without_discount = Decimal('0.20') * self.price + self.price
+        return price_without_discount
+
+    def calculate_tax(self):
+        return self.price * Decimal('0.05')
+
+    def calculate_shipping_cost(self, weight: Decimal):
+        return weight * Decimal('1.50')
+
+    def format_product_name(self):
+        return f"Discounted Product: {self.name}"
+
+
+# Superhero Universe
+class RechargeEnergyMixin:
+    def recharge_energy(self, amount: int):
+        self.energy = min(self.energy + amount, 100)
+        self.save()
+
+
+class Hero(RechargeEnergyMixin, models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+    hero_title = models.CharField(
+        max_length=100,
+    )
+    energy = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1)
+        ]
+    )
+
+
+class SpiderHero(Hero):
+    def swing_from_buildings(self):
+        if self.energy < 80:
+            return f"{self.name} as Spider Hero is out of web shooter fluid"
+
+        self.energy = max(self.energy - 80, 1)
+        self.save()
+        return f"{self.name} as Spider Hero swings from buildings using web shooters"
+
+    class Meta:
+        proxy = True
+
+
+class FlashHero(Hero):
+    def run_at_super_speed(self):
+        if self.energy < 65:
+            return f"{self.name} as Flash Hero needs to recharge the speed force"
+
+        self.energy = max(self.energy - 65, 1)
+        self.save()
+        return f"{self.name} as Flash Hero runs at lightning speed, saving the day"
+
+    class Meta:
+        proxy = True

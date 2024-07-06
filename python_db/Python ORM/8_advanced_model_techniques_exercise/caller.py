177f76ded452b4292220998a15df16382c9791a2
Ivaylo Arsov <125567658+IvayloArsov@users.noreply.github.com>
2024-07-06 13:13:51 +0300

    asdf

--- python_db/Python ORM/8_advanced_model_techniques_exercise/caller.py
+++ python_db/Python ORM/8_advanced_model_techniques_exercise/caller.py
@@ -1,5 +1,8 @@
 import os
 import django
+from django.core.exceptions import ValidationError
+
+from main_app.models import Book
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
--- python_db/Python ORM/8_advanced_model_techniques_exercise/main_app/models.py
+++ python_db/Python ORM/8_advanced_model_techniques_exercise/main_app/models.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ValidationError
-from django.core.validators import MaxLengthValidator, MinValueValidator, EmailValidator, URLValidator
+from django.core.validators import MaxLengthValidator, MinValueValidator, EmailValidator, URLValidator, \
+    MinLengthValidator
 from django.db import models
 
 
@@ -45,5 +46,65 @@ class Customer(models.Model):
         }
     )
 
+
 # Media task
-asdf
+
+class BaseMedia(models.Model):
+    title = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField()
+    genre = models.CharField(
+        max_length=50,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at', 'title']
+
+
+class Book(BaseMedia):
+    class Meta(BaseMedia.Meta):
+        verbose_name = f'Model Book'
+        verbose_name_plural = f'Models of type - Book'
+
+    author = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(5, 'Author must be at least 5 characters long')
+        ]
+    )
+    isbn = models.CharField(
+        unique=True,
+        max_length=20,
+        validators=[
+            MinLengthValidator(6, 'ISBN must be at least 6 characters long')
+        ]
+    )
+
+
+class Movie(BaseMedia):
+    class Meta(BaseMedia.Meta):
+        verbose_name = f'Model Movie'
+        verbose_name_plural = f'Models of type - Movie'
+
+    director = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(8, 'Director must be at least 8 characters long')
+        ]
+    )
+
+
+class Music(BaseMedia):
+    class Meta(BaseMedia.Meta):
+        verbose_name = f'Model Music'
+        verbose_name_plural = f'Models of type - Music'
+
+    artist = models.CharField(
+        max_length=100,
+        validators=[
+            MinLengthValidator(9, 'Artist must be at least 9 characters long')
+        ]
+    )
